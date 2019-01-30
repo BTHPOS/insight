bb851aade184322360fff6dce236c0734a991cb9
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-29 19:22:37 -0500

    Updated style and fixed USD errors

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'BTG';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'BTH';
 
 angular.module('insight',[
   'ngAnimate',
--- public/src/js/controllers/header.js
+++ public/src/js/controllers/header.js
@@ -6,8 +6,8 @@ angular.module('insight.system').controller('HeaderController',
 
     $rootScope.currency = {
       factor: 1,
-      symbol: 'BTG',
-      realSymbol: 'BTG'
+      symbol: 'BTH',
+      realSymbol: 'BTH'
     };
 
     $scope.menu = [{
